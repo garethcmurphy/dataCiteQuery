195f10372f5d8a3bdb6cad0051880e9bcd523975
Gareth Murphy <garethcmurphy@gmail.com>
2019-05-28 11:43:51 +0200

    first commit

---
+++ x.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+
+_transport = RequestsHTTPTransport(
+    url='https://api.datacite.org/graphql',
+    use_json=True,
+)
+
+
+client = Client(
+    transport=_transport,
+    fetch_schema_from_transport=True,
+)
+query = gql( """{
+   researcher(id: "https://orcid.org/0000-0003-1419-2405") {
+    id
+    name
+    publications(first: 50) {
+      totalCount
+      nodes {
+        id
+        relatedIdentifiers {
+          relatedIdentifier
+        }
+      }
+    }
+  }
+}""")
+data=client.execute(query)
+print(data["researcher"]["name"])
+
+pd.read_json(data)
+#print(data.researcher.name)
